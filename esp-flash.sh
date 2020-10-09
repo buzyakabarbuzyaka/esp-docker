8a4303198d2d06069656b0f8343ac00315ab627c
Buzov Vyahceslav <buzovv1997@gmail.com>
2020-10-09 20:37:08 +0300

    esp flash script added

---
+++ esp-flash.sh
@@ -0,0 +1,14 @@
+python ./arduino-esp32/tools/esptool/esptool.py \
+--chip esp32 \
+--port /dev/ttyUSB0 \
+--baud 921600 \
+--before default_reset \
+--after hard_reset \
+write_flash -z \
+--flash_mode dio \
+--flash_freq 40m \
+--flash_size detect \
+0xe000 ./arduino-esp32/tools/partitions/boot_app0.bin \
+0x1000 ./arduino-esp32/tools/sdk/bin/bootloader_dio_40m.bin \
+0x10000 ./output/app.bin \
+0x8000 ./output/app.partitions.bin
